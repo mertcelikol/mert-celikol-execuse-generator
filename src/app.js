a820c4e82fcb0120dc48a0470531ea17ab63a799
Mert çelikol <74824635+mertcelikol@users.noreply.github.com>
2024-09-19 01:03:05 +0000

    done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // let randomNumber = Math.random();
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomaction = action[Math.floor(Math.random() * action.length)];
+  let randomwhat = what[Math.floor(Math.random() * what.length)];
+  let randomwhen = when[Math.floor(Math.random() * when.length)];
+
+  document.querySelector(".execuse").innerHTML =
+    randomWho + randomaction + randomwhat + randomwhen;
+  console.log(randomWho + randomaction + randomwhat + randomwhen);
 };
+
+/* create a function random generator whole number for arrays
+ */
